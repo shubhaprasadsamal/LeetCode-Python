03e88ce05b4af51188037dacc0235af5c9f65702
Shitansu Shubha <shubhaprasadsamal@gmail.com>
2021-02-20 12:58:03 -0800

    sql

--- LeetCode/sql/leetcode_sqlSolution.sql
+++ LeetCode/sql/leetcode_sqlSolution.sql
@@ -1210,48 +1210,49 @@ group by 1
 order by 1;
 
 
-1173. Immediate Food Delivery I                                 [Revision]
-Table: Delivery
+--1173. Immediate Food Delivery I                                 [Revision]
+--Table: Delivery
+--
+--+-----------------------------+---------+
+--| Column Name                 | Type    |
+--+-----------------------------+---------+
+--| delivery_id                 | int     |
+--| customer_id                 | int     |
+--| order_date                  | date    |
+--| customer_pref_delivery_date | date    |
+--+-----------------------------+---------+
+--delivery_id is the primary key of this table.
+--The table holds information about food delivery to customers that make orders at some date and specify a preferred delivery date (on the same order date or after it).
+--
+--
+--If the preferred delivery date of the customer is the same as the order date then the order is called immediate otherwise it's called scheduled.
+--
+--Write an SQL query to find the percentage of immediate orders in the table, rounded to 2 decimal places.
+--
+--The query result format is in the following example:
+--
+--Delivery table:
+--+-------------+-------------+------------+-----------------------------+
+--| delivery_id | customer_id | order_date | customer_pref_delivery_date |
+--+-------------+-------------+------------+-----------------------------+
+--| 1           | 1           | 2019-08-01 | 2019-08-02                  |
+--| 2           | 5           | 2019-08-02 | 2019-08-02                  |
+--| 3           | 1           | 2019-08-11 | 2019-08-11                  |
+--| 4           | 3           | 2019-08-24 | 2019-08-26                  |
+--| 5           | 4           | 2019-08-21 | 2019-08-22                  |
+--| 6           | 2           | 2019-08-11 | 2019-08-13                  |
+--+-------------+-------------+------------+-----------------------------+
+--
+--Result table:
+--+----------------------+
+--| immediate_percentage |
+--+----------------------+
+--| 33.33                |
+--+----------------------+
+--The orders with delivery id 2 and 3 are immediate while the others are scheduled.
+--
+--MySQL:
 
-+-----------------------------+---------+
-| Column Name                 | Type    |
-+-----------------------------+---------+
-| delivery_id                 | int     |
-| customer_id                 | int     |
-| order_date                  | date    |
-| customer_pref_delivery_date | date    |
-+-----------------------------+---------+
-delivery_id is the primary key of this table.
-The table holds information about food delivery to customers that make orders at some date and specify a preferred delivery date (on the same order date or after it).
-
-
-If the preferred delivery date of the customer is the same as the order date then the order is called immediate otherwise it's called scheduled.
-
-Write an SQL query to find the percentage of immediate orders in the table, rounded to 2 decimal places.
-
-The query result format is in the following example:
-
-Delivery table:
-+-------------+-------------+------------+-----------------------------+
-| delivery_id | customer_id | order_date | customer_pref_delivery_date |
-+-------------+-------------+------------+-----------------------------+
-| 1           | 1           | 2019-08-01 | 2019-08-02                  |
-| 2           | 5           | 2019-08-02 | 2019-08-02                  |
-| 3           | 1           | 2019-08-11 | 2019-08-11                  |
-| 4           | 3           | 2019-08-24 | 2019-08-26                  |
-| 5           | 4           | 2019-08-21 | 2019-08-22                  |
-| 6           | 2           | 2019-08-11 | 2019-08-13                  |
-+-------------+-------------+------------+-----------------------------+
-
-Result table:
-+----------------------+
-| immediate_percentage |
-+----------------------+
-| 33.33                |
-+----------------------+
-The orders with delivery id 2 and 3 are immediate while the others are scheduled.
-
-MySQL:
 select
     round((count(d1.delivery_id)/count(d2.delivery_id))*100,2) immediate_percentage
 from
@@ -1261,7 +1262,7 @@ on
 and
     d1.order_date = d2.customer_pref_delivery_date;
 
-Explain:
+--Explain:
 select
     d1.delivery_id,
     d1.customer_id,
@@ -1285,12 +1286,3 @@ and
 		[null, 		null, 		null, 		null, 				4, 			3, 		"2019-08-24", 		"2019-08-26"],
 		[null, 		null, 		null, 		null, 				5, 			4, 		"2019-08-21", 		"2019-08-22"],
 		[null, 		null, 		null, 		null, 				6, 			2, 		"2019-08-11", 		"2019-08-13"]
-
-	["d1.delivery_id", "d1.customer_id", "d1.order_date", "d1.customer_pref_delivery_date", "d2.delivery_id", "d2.customer_id", "d2.order_date", "d2.customer_pref_delivery_date"]
-		[null, 		    null, 		        null, 		    null, 				                1, 			            1, 		"2019-08-01", 		"2019-08-02"],
-		[2, 		    5, 		            "2019-08-02", 	"2019-08-02", 			            2, 			            5, 		"2019-08-02", 		"2019-08-02"],
-		[3, 		    1, 		            "2019-08-11", 	"2019-08-11", 			            3, 			            1, 		"2019-08-11", 		"2019-08-11"],
-		[null, 		    null, 		        null, 		    null, 				                4, 			            3, 		"2019-08-24", 		"2019-08-26"],
-		[null, 		    null, 		        null, 		    null, 				                5, 			            4, 		"2019-08-21", 		"2019-08-22"],
-		[null, 		    null, 		        null, 		    null, 				                6, 			            2, 		"2019-08-11", 		"2019-08-13"]
-
