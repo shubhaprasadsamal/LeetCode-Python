5278ecb610cda67513b07a33a76b00013df16067
Shitansu Shubha <shubhaprasadsamal@gmail.com>
2021-02-22 20:55:18 -0800

    sql

--- LeetCode/sql/leetcode_sqlSolution.sql
+++ LeetCode/sql/leetcode_sqlSolution.sql
@@ -3388,27 +3388,27 @@ where
 ;
 
 
-182. Duplicate Emails       [Revision]
-Write a SQL query to find all duplicate emails in a table named Person.
-
-+----+---------+
-| Id | Email   |
-+----+---------+
-| 1  | a@b.com |
-| 2  | c@d.com |
-| 3  | a@b.com |
-+----+---------+
-For example, your query should return the following for the above table:
-
-+---------+
-| Email   |
-+---------+
-| a@b.com |
-+---------+
-Note: All emails are in lowercase
-
-
-MySQL:
+--182. Duplicate Emails       [Revision]
+--Write a SQL query to find all duplicate emails in a table named Person.
+--
+--+----+---------+
+--| Id | Email   |
+--+----+---------+
+--| 1  | a@b.com |
+--| 2  | c@d.com |
+--| 3  | a@b.com |
+--+----+---------+
+--For example, your query should return the following for the above table:
+--
+--+---------+
+--| Email   |
+--+---------+
+--| a@b.com |
+--+---------+
+--Note: All emails are in lowercase
+--
+--
+--MySQL:
 
 select
     distinct p1.Email "Email"
