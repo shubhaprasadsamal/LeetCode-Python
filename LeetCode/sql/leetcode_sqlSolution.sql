3db9ebb6149442999cac3e03d7d6db42bfe027be
Shitansu Shubha <shubhaprasadsamal@gmail.com>
2021-03-11 17:34:14 -0800

    sql

--- LeetCode/sql/leetcode_sqlSolution.sql
+++ LeetCode/sql/leetcode_sqlSolution.sql
@@ -1983,7 +1983,7 @@ group by
 where
     rnk = 1;
 
---1280. Students and Examinations   [Revision]
+--1280. Students and Examinations   [Revision ]
 --Table: Students
 --
 --+---------------+---------+
