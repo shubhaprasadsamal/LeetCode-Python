b14bf06bd1e9d060d94b66e54ada9c3bd9a19bc7
Shitansu Shubha <shubhaprasadsamal@gmail.com>
2021-03-20 10:58:11 -0700

    sql

--- LeetCode/python/trees.py
+++ LeetCode/python/trees.py
@@ -292,43 +292,5 @@ class Solution:
         return avg
 
 #
-# 257. Binary Tree Paths
-# Given a binary tree, return all root-to-leaf paths.
-#
-# Note: A leaf is a node with no children.
-#
-# Example:
-#
-# Input:
-#
-# 1
-# / \
-#     2     3
-# \
-# 5
-#
-# Output: ["1->2->5", "1->3"]
-#
-# Explanation: All root-to-leaf paths are: 1->2->5, 1->3
-#
-# Solutions:
-class Solution:
-    def binaryTreePaths(self, root: TreeNode) -> List[str]:
-
-        def construct_path(root,path):
-            if root:
-                path = path+str(root.val)
-
-                if  not root.left and not root.right:
-                    paths.append(path)
-                else:
-                    path = path + "->"
-                    construct_path(root.left,path)
-                    construct_path(root.right,path)
-
-        paths = []
-        construct_path(root,"")
-        return paths
-
 #
 
