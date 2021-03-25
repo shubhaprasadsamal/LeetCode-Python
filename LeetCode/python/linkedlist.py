26e83de422c5c8c909ff73b973bb3dbc75dc9480
Shitansu Shubha <shubhaprasadsamal@gmail.com>
2021-03-24 21:11:44 -0700

    sql

--- LeetCode/python/linkedlist.py
+++ LeetCode/python/linkedlist.py
@@ -28,3 +28,30 @@ class Solution:             # O(n) and O(n)
 
         return res == res[::-1]
 
+# 206. Reverse Linked List
+# Reverse a singly linked list.
+#
+# Example:
+#
+# Input: 1->2->3->4->5->NULL
+# Output: 5->4->3->2->1->NULL
+# Follow up:
+#
+# A linked list can be reversed either iteratively or recursively. Could you implement both?
+#
+# Solution:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+
+        while head:
+            temp = head
+            head = head.next
+            temp.next = prev
+            prev = temp
+        return prev
