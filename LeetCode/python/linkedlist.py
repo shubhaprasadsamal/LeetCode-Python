905cc67f3a810ab4515819460de7c107bc8c8d77
Shitansu Shubha <shubhaprasadsamal@gmail.com>
2021-03-25 17:13:50 -0700

    python

--- LeetCode/python/linkedlist.py
+++ LeetCode/python/linkedlist.py
@@ -58,7 +58,8 @@ class Solution:
 
 
 # 21. Merge Two Sorted Lists
-# Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+# Merge two sorted linked lists and return it as a sorted list.
+# The list should be made by splicing together the nodes of the first two lists.
 #
 #
 #
