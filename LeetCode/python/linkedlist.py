285bbcdb18a1250e06b75711f85321ed9d7f299c
Shitansu Shubha <shubhaprasadsamal@gmail.com>
2021-03-25 17:02:42 -0700

    python

--- LeetCode/python/linkedlist.py
+++ LeetCode/python/linkedlist.py
@@ -55,3 +55,49 @@ class Solution:
             temp.next = prev
             prev = temp
         return prev
+
+
+# 21. Merge Two Sorted Lists
+# Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: l1 = [1,2,4], l2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+# Example 2:
+#
+# Input: l1 = [], l2 = []
+# Output: []
+# Example 3:
+#
+# Input: l1 = [], l2 = [0]
+# Output: [0]
+
+# Solution:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        prehead = ListNode(-1)
+
+        prev = prehead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+
+        # At least one of l1 and l2 can still have nodes at this point, so connect
+        # the non-null list to the end of the merged list.
+        prev.next = l1 if l1 is not None else l2
+
+        return prehead.next
