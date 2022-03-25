25761a8dacc1451866bf9ff62b66cb0806f9f87b
Shitansu <lat61300@adobe.com>
2022-03-25 11:05:08 -0700

    Bits

--- bitOperator/bitOperator.py
+++ bitOperator/bitOperator.py
@@ -95,4 +95,78 @@ a	  a << n (Bitwise left shift)
 12 = 1100
 12 >> 2 = 110000 (48)
 
+
+How can we use this one in Program/Algorithm:
+
+when we say 3 << 1, it's nothing but 3 * 2 in python and gives the same result
+3 = 11
+6 = 110
+12 = 1100
+hence in the loop where ever we write 3 * 2, we can use this bit wise operator like 3 << 1
+
+
+ODD or EVEN Number:
+###################
+
+Look at the below numbers and their corresponding binary:
+Even:
+
+2 = 10
+4 = 100
+6 = 110
+
+Odd:
+
+3 = 11
+5 = 101
+7 = 111
+
+Hence, we noticed that all the even numbers ends with 0 and all the odd number ends with 1.
+
+In order to find even or End we can use masking
+Here the masking example that we are going to use is an & operation between number and 1 to find even/odd
+
+7 = 111
+
+  111
+& 001
+------
+  001 <= if the right most number is 1 then 7 is a odd number
+
+6 = 110
+
+  110
+& 001
+------
+  000 <= if the right most number is 0 then 6 is a even number
+
+if a&1 == 0:
+Number is even
+else:
+number is odd
+
+
+Swap Two Numbers:
+#################
+
+a = 5
+b = 7
+
+result: a = 7; b = 5
+
+temp = a
+a = b
+b = temp
+
+In bitwise way:
+
+a = 5 = 101
+b = 7 = 111
+
+a = a ^ b (XOR) [101 ^ 111 = 010 (2)]
+b = a ^ b (XOR) [010 ^ 111 = 101 (5)]
+b = a ^ b (XOR) [010 ^ 101 = 111 (7)]
+
+
+
 """
