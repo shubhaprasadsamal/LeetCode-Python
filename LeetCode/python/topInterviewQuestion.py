39dbad0dd9084b81f161f3a7474f8b4ea3760b85
Shitansu <lat61300@adobe.com>
2022-07-08 10:25:17 -0700

    Tree

--- LeetCode/python/topInterviewQuestion.py
+++ LeetCode/python/topInterviewQuestion.py
@@ -419,3 +419,44 @@ while i < len(nums1) and j < len(nums2):
         j+=1
 
 return result
+
+# 94. Binary Tree Inorder Traversal
+# Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+# Example 1:
+#
+# Input: root = [1,null,2,3]
+# Output: [1,3,2]
+# Example 2:
+#
+# Input: root = []
+# Output: []
+# Example 3:
+#
+# Input: root = [1]
+# Output: [1]
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        output = []
+        def inorder(root):
+            if not root:
+                return output
+            inorder(root.left)
+            print("root: ",root)
+            output.append(root.val)
+            inorder(root.right)
+
+        inorder(root)
+        return output
+
