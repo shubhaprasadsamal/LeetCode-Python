8fff7cb2c17a3b60601516647c19e28ea800ca09
Shitansu <lat61300@adobe.com>
2022-07-07 20:13:04 -0700

    Basics

---
+++ interviewQuestions/iQuestions.py
@@ -0,0 +1,30 @@
+# 1. strs = "I am a boy"
+# output = "boy a am I"
+
+def reverse(strs):
+    lst = strs.split()
+    print("Lsts: ", lst)
+    strs = " ".join(lst[::-1])
+    return strs
+
+strs = "I am a boy"
+print(reverse(strs)) # boy a am I
+
+# 2. phone = '3265847308'
+# find the sum wich is going to be the addition of repetative numbers.
+# sum = 3 + 8 = 11
+
+def addition(strs):
+    dix = {}
+    sum  =0
+    for i in strs:
+        if i in dix:
+            sum += int(i)
+        else:
+            dix[i] = 1
+    return sum
+
+strs = '3265847308'
+print(addition(strs)) # 11
+
+
