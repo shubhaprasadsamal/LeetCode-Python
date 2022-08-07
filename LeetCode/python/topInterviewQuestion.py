710602b669870b5e300cc91c402d30efe4a45521
Shitansu <lat61300@adobe.com>
2022-08-07 10:49:09 -0700

    Tree

--- LeetCode/python/topInterviewQuestion.py
+++ LeetCode/python/topInterviewQuestion.py
@@ -1,3 +1,57 @@
+# 412. Fizz Buzz
+# Given an integer n, return a string array answer (1-indexed) where:
+#
+# answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+# answer[i] == "Fizz" if i is divisible by 3.
+# answer[i] == "Buzz" if i is divisible by 5.
+# answer[i] == i (as a string) if none of the above conditions are true.
+#
+#
+# Example 1:
+#
+# Input: n = 3
+# Output: ["1","2","Fizz"]
+# Example 2:
+#
+# Input: n = 5
+# Output: ["1","2","Fizz","4","Buzz"]
+# Example 3:
+#
+# Input: n = 15
+# Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        output = []
+        for i in range(1,n+1):
+            if i%3 == 0 and i%5 == 0:
+                output.append("FizzBuzz")
+            elif i%3 == 0:
+                output.append("Fizz")
+            elif i%5 == 0:
+                output.append("Buzz")
+            else:
+                output.append(str(i))
+        return output
+
+# 14. Longest Common Prefix
+# Write a function to find the longest common prefix string amongst an array of strings.
+#
+# If there is no common prefix, return an empty string "".
+#
+#
+#
+# Example 1:
+#
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+# Example 2:
+#
+# Input: strs = ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+
+
 # 108. Convert Sorted Array to Binary Search Tree
 # Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
 #
