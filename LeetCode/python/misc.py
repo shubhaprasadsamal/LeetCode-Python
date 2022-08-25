94fde13e914d7e5fb0d06f33ad616965b50989c1
Shitansu <lat61300@adobe.com>
2022-08-24 19:42:40 -0700

    Tree

--- LeetCode/python/misc.py
+++ LeetCode/python/misc.py
@@ -5,8 +5,11 @@
 #     print(c)
 #     return b
 
-a = [1,2,3,4]
-b = ['a','b','c','d']
-c = zip(b,a)
-print(tuple(c)) # (('a', 1), ('b', 2), ('c', 3), ('d', 4))
-print(dict(zip(b,a))) # {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+# a = [1,2,3,4]
+# b = ['a','b','c','d']
+# c = zip(b,a)
+# print(tuple(c)) # (('a', 1), ('b', 2), ('c', 3), ('d', 4))
+# print(dict(zip(b,a))) # {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+a = [1,2,3,4,5,6]
+print(a[2:4])
