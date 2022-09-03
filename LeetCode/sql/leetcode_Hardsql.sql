159709a72e5200452d271e5d5e8a73eea37a184c
Shitansu <lat61300@adobe.com>
2022-09-03 15:58:29 -0700

    sql

--- LeetCode/sql/leetcode_Hardsql.sql
+++ LeetCode/sql/leetcode_Hardsql.sql
@@ -841,3 +841,108 @@ from
 on
    num = transactions_cnt ;
 
+--1369. Get the Second Most Recent Activity
+--Hard
+--
+--122
+--
+--12
+--
+--Add to List
+--
+--Share
+--SQL Schema
+--Table: UserActivity
+--
+--+---------------+---------+
+--| Column Name   | Type    |
+--+---------------+---------+
+--| username      | varchar |
+--| activity      | varchar |
+--| startDate     | Date    |
+--| endDate       | Date    |
+--+---------------+---------+
+--There is no primary key for this table. It may contain duplicates.
+--This table contains information about the activity performed by each user in a period of time.
+--A person with username performed an activity from startDate to endDate.
+--
+--
+--Write an SQL query to show the second most recent activity of each user.
+--
+--If the user only has one activity, return that one. A user cannot perform more than one activity at the same time.
+--
+--Return the result table in any order.
+--
+--The query result format is in the following example.
+--
+--
+--
+--Example 1:
+--
+--Input:
+--UserActivity table:
+--+------------+--------------+-------------+-------------+
+--| username   | activity     | startDate   | endDate     |
+--+------------+--------------+-------------+-------------+
+--| Alice      | Travel       | 2020-02-12  | 2020-02-20  |
+--| Alice      | Dancing      | 2020-02-21  | 2020-02-23  |
+--| Alice      | Travel       | 2020-02-24  | 2020-02-28  |
+--| Bob        | Travel       | 2020-02-11  | 2020-02-18  |
+--+------------+--------------+-------------+-------------+
+--Output:
+--+------------+--------------+-------------+-------------+
+--| username   | activity     | startDate   | endDate     |
+--+------------+--------------+-------------+-------------+
+--| Alice      | Dancing      | 2020-02-21  | 2020-02-23  |
+--| Bob        | Travel       | 2020-02-11  | 2020-02-18  |
+--+------------+--------------+-------------+-------------+
+--Explanation:
+--The most recent activity of Alice is Travel from 2020-02-24 to 2020-02-28, before that she was dancing from 2020-02-21 to 2020-02-23.
+--Bob only has one record, we just take that one.
+# Write your MySQL query statement below
+with cte1 as
+(
+select
+    username,
+    activity,
+    startDate,
+    endDate,
+    dense_rank() over (partition by username order by startDate desc,endDate desc) as rnk
+from
+    UserActivity
+)
+# select * from cte1
+,
+cte2 as
+(
+select
+    username,
+    max(rnk) as max_rnk
+from
+    cte1
+group by
+    1
+)
+select
+    username,
+    activity,
+    startDate,
+    endDate
+from
+    cte1
+where
+    rnk = 2
+union all
+select
+    username,
+    activity,
+    startDate,
+    endDate
+from
+    cte1
+where
+    username in (
+                    select username from cte2 where max_rnk<2
+)
+;
+
