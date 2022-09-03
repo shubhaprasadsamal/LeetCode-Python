7f421de66846ce4731004303c396c30af118f014
Shitansu <lat61300@adobe.com>
2022-09-03 10:20:54 -0700

    sql

--- LeetCode/sql/leetcode_Hardsql.sql
+++ LeetCode/sql/leetcode_Hardsql.sql
@@ -544,3 +544,148 @@ and
 group by
     1
 
+--1225. Report Contiguous Dates
+--Table: Failed
+--
+--+--------------+---------+
+--| Column Name  | Type    |
+--+--------------+---------+
+--| fail_date    | date    |
+--+--------------+---------+
+--fail_date is the primary key for this table.
+--This table contains the days of failed tasks.
+--
+--
+--Table: Succeeded
+--
+--+--------------+---------+
+--| Column Name  | Type    |
+--+--------------+---------+
+--| success_date | date    |
+--+--------------+---------+
+--success_date is the primary key for this table.
+--This table contains the days of succeeded tasks.
+--
+--
+--A system is running one task every day. Every task is independent of the previous tasks. The tasks can fail or succeed.
+--
+--Write an SQL query to generate a report of period_state for each continuous interval of days in the period from 2019-01-01 to 2019-12-31.
+--
+--period_state is 'failed' if tasks in this interval failed or 'succeeded' if tasks in this interval succeeded. Interval of days are retrieved as start_date and end_date.
+--
+--Return the result table ordered by start_date.
+--
+--The query result format is in the following example.
+--
+--
+--
+--Example 1:
+--
+--Input:
+--Failed table:
+--+-------------------+
+--| fail_date         |
+--+-------------------+
+--| 2018-12-28        |
+--| 2018-12-29        |
+--| 2019-01-04        |
+--| 2019-01-05        |
+--+-------------------+
+--Succeeded table:
+--+-------------------+
+--| success_date      |
+--+-------------------+
+--| 2018-12-30        |
+--| 2018-12-31        |
+--| 2019-01-01        |
+--| 2019-01-02        |
+--| 2019-01-03        |
+--| 2019-01-06        |
+--+-------------------+
+--Output:
+--+--------------+--------------+--------------+
+--| period_state | start_date   | end_date     |
+--+--------------+--------------+--------------+
+--| succeeded    | 2019-01-01   | 2019-01-03   |
+--| failed       | 2019-01-04   | 2019-01-05   |
+--| succeeded    | 2019-01-06   | 2019-01-06   |
+--+--------------+--------------+--------------+
+--Explanation:
+--The report ignored the system state in 2018 as we care about the system in the period 2019-01-01 to 2019-12-31.
+--From 2019-01-01 to 2019-01-03 all tasks succeeded and the system state was "succeeded".
+--From 2019-01-04 to 2019-01-05 all tasks failed and the system state was "failed".
+--From 2019-01-06 to 2019-01-06 all tasks succeeded and the system state was "succeeded".
+
+-- WHAT"S WRONG WITH THIS SOLUTION??
+--SOLUTION-1
+# Write your MySQL query statement below
+
+with cte as
+(
+select
+    "failed" as state ,
+    fail_date as event_dt
+from
+    Failed
+where
+    YEAR(fail_date) = '2019'
+union all
+select
+    "succeeded" as state,
+    success_date  as event_dt
+from
+    Succeeded
+where
+    YEAR(success_date) = '2019'
+)
+,cte_group_dt as
+(
+select
+    *,row_number() over(order by event_dt) as rnk
+from
+    cte
+)
+,cte_category_conv as
+(
+select
+    *,row_number() over(partition by state order by event_dt) as rnk_category
+from
+    cte_group_dt
+), cte_category as
+(
+select
+    *,rnk-rnk_category as category
+from
+    cte_category_conv
+)
+# select * from cte_category
+select
+    max(state) as period_state,
+    min(event_dt) as start_date,
+    max(event_dt) as end_date
+from
+    cte_category
+group by
+    category
+order by
+    2
+
+--SOLUTION-2
+# Write your MySQL query statement below
+
+with x as (
+select fail_date as dat, 'failed'  as period_state, rank() over(order by fail_date) as rnk
+from Failed
+where year(fail_date)=2019
+union all
+select success_date as dat, 'succeeded' as period_state, rank() over(order by success_date) as rnk
+from Succeeded
+where year(success_date)=2019
+    )
+
+Select period_state, min(dat) as start_date, max(dat) as end_date
+ from (
+ select *, (rank() over(order by dat)-rnk) as group_rnk from x
+     ) y
+group by period_state, group_rnk
+order by 2
