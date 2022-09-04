2c90cb383d323fba12a2341e15867ff807520e8d
Shitansu <lat61300@adobe.com>
2022-09-03 23:44:25 -0700

    sql

--- LeetCode/sql/leetcode_mediumsql.sql
+++ LeetCode/sql/leetcode_mediumsql.sql
@@ -292,3 +292,201 @@ from
 where
     salary not in (select max(salary) from Employee);
 
+--177. Nth Highest Salary
+--Table: Employee
+--
+--+-------------+------+
+--| Column Name | Type |
+--+-------------+------+
+--| id          | int  |
+--| salary      | int  |
+--+-------------+------+
+--id is the primary key column for this table.
+--Each row of this table contains information about the salary of an employee.
+--
+--
+--Write an SQL query to report the nth highest salary from the Employee table. If there is no nth highest salary, the query should report null.
+--
+--The query result format is in the following example.
+--
+--
+--
+--Example 1:
+--
+--Input:
+--Employee table:
+--+----+--------+
+--| id | salary |
+--+----+--------+
+--| 1  | 100    |
+--| 2  | 200    |
+--| 3  | 300    |
+--+----+--------+
+--n = 2
+--Output:
+--+------------------------+
+--| getNthHighestSalary(2) |
+--+------------------------+
+--| 200                    |
+--+------------------------+
+--Example 2:
+--
+--Input:
+--Employee table:
+--+----+--------+
+--| id | salary |
+--+----+--------+
+--| 1  | 100    |
+--+----+--------+
+--n = 2
+--Output:
+--+------------------------+
+--| getNthHighestSalary(2) |
+--+------------------------+
+--| null                   |
+--+------------------------+
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      with cte as
+      (
+      select
+        salary,
+        dense_rank() over(order by salary desc) as rnk
+      from
+        Employee
+          )
+      select
+        salary as getNthHighestSalary
+      from
+        cte
+      where
+        rnk = n
+      limit 1
+
+  );
+END
+
+--178. Rank Scores
+--
+--Table: Scores
+--
+--+-------------+---------+
+--| Column Name | Type    |
+--+-------------+---------+
+--| id          | int     |
+--| score       | decimal |
+--+-------------+---------+
+--id is the primary key for this table.
+--Each row of this table contains the score of a game. Score is a floating point value with two decimal places.
+--
+--
+--Write an SQL query to rank the scores. The ranking should be calculated according to the following rules:
+--
+--The scores should be ranked from the highest to the lowest.
+--If there is a tie between two scores, both should have the same ranking.
+--After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
+--Return the result table ordered by score in descending order.
+--
+--The query result format is in the following example.
+--
+--
+--
+--Example 1:
+--
+--Input:
+--Scores table:
+--+----+-------+
+--| id | score |
+--+----+-------+
+--| 1  | 3.50  |
+--| 2  | 3.65  |
+--| 3  | 4.00  |
+--| 4  | 3.85  |
+--| 5  | 4.00  |
+--| 6  | 3.65  |
+--+----+-------+
+--Output:
+--+-------+------+
+--| score | rank |
+--+-------+------+
+--| 4.00  | 1    |
+--| 4.00  | 1    |
+--| 3.85  | 2    |
+--| 3.65  | 3    |
+--| 3.65  | 3    |
+--| 3.50  | 4    |
+--+-------+------+
+
+# Write your MySQL query statement below
+
+select
+    score ,
+    dense_rank() over(order by score desc) as 'rank'
+from
+    Scores ;
+
+--180. Consecutive Numbers
+--Medium
+--
+--1181
+--
+--207
+--
+--Add to List
+--
+--Share
+--SQL Schema
+--Table: Logs
+--
+--+-------------+---------+
+--| Column Name | Type    |
+--+-------------+---------+
+--| id          | int     |
+--| num         | varchar |
+--+-------------+---------+
+--id is the primary key for this table.
+--id is an autoincrement column.
+--
+--
+--Write an SQL query to find all numbers that appear at least three times consecutively.
+--
+--Return the result table in any order.
+--
+--The query result format is in the following example.
+--
+--
+--
+--Example 1:
+--
+--Input:
+--Logs table:
+--+----+-----+
+--| id | num |
+--+----+-----+
+--| 1  | 1   |
+--| 2  | 1   |
+--| 3  | 1   |
+--| 4  | 2   |
+--| 5  | 1   |
+--| 6  | 2   |
+--| 7  | 2   |
+--+----+-----+
+--Output:
+--+-----------------+
+--| ConsecutiveNums |
+--+-----------------+
+--| 1               |
+--+-----------------+
+--Explanation: 1 is the only number that appears consecutively for at least three times.
+
+# Write your MySQL query statement below
+select
+    distinct l1.num  as "ConsecutiveNums"
+from
+    Logs l1
+    inner join Logs l2 on (l1.num = l2.num and l1.id = l2.id - 1)
+    inner join Logs l3 on (l1.num = l3.num and l1.id = l3.id - 2)
+;
