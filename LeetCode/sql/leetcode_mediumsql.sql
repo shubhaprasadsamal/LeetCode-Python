102f086bd18089caba5a072e7ecbe6dc1b1cac01
Shitansu <lat61300@adobe.com>
2022-09-05 23:57:36 -0700

    sql

--- LeetCode/sql/leetcode_mediumsql.sql
+++ LeetCode/sql/leetcode_mediumsql.sql
@@ -2889,4 +2889,115 @@ order by
 limit 1
 )
 select person_name from cte1;
+--1205. Monthly Transactions II
+--Medium
+--
+--118
+--
+--438
+--
+--Add to List
+--
+--Share
+--SQL Schema
+--Table: Transactions
+--
+--+----------------+---------+
+--| Column Name    | Type    |
+--+----------------+---------+
+--| id             | int     |
+--| country        | varchar |
+--| state          | enum    |
+--| amount         | int     |
+--| trans_date     | date    |
+--+----------------+---------+
+--id is the primary key of this table.
+--The table has information about incoming transactions.
+--The state column is an enum of type ["approved", "declined"].
+--Table: Chargebacks
+--
+--+----------------+---------+
+--| Column Name    | Type    |
+--+----------------+---------+
+--| trans_id       | int     |
+--| trans_date     | date    |
+--+----------------+---------+
+--Chargebacks contains basic information regarding incoming chargebacks from some transactions placed in Transactions table.
+--trans_id is a foreign key to the id column of Transactions table.
+--Each chargeback corresponds to a transaction made previously even if they were not approved.
+--
+--
+--Write an SQL query to find for each month and country: the number of approved transactions and their total amount, the number of chargebacks, and their total amount.
+--
+--Note: In your query, given the month and country, ignore rows with all zeros.
+--
+--Return the result table in any order.
+--
+--The query result format is in the following example.
+--
+--
+--
+--Example 1:
+--
+--Input:
+--Transactions table:
+--+-----+---------+----------+--------+------------+
+--| id  | country | state    | amount | trans_date |
+--+-----+---------+----------+--------+------------+
+--| 101 | US      | approved | 1000   | 2019-05-18 |
+--| 102 | US      | declined | 2000   | 2019-05-19 |
+--| 103 | US      | approved | 3000   | 2019-06-10 |
+--| 104 | US      | declined | 4000   | 2019-06-13 |
+--| 105 | US      | approved | 5000   | 2019-06-15 |
+--+-----+---------+----------+--------+------------+
+--Chargebacks table:
+--+----------+------------+
+--| trans_id | trans_date |
+--+----------+------------+
+--| 102      | 2019-05-29 |
+--| 101      | 2019-06-30 |
+--| 105      | 2019-09-18 |
+--+----------+------------+
+--Output:
+--+---------+---------+----------------+-----------------+------------------+-------------------+
+--| month   | country | approved_count | approved_amount | chargeback_count | chargeback_amount |
+--+---------+---------+----------------+-----------------+------------------+-------------------+
+--| 2019-05 | US      | 1              | 1000            | 1                | 2000              |
+--| 2019-06 | US      | 2              | 8000            | 1                | 1000              |
+--| 2019-09 | US      | 0              | 0               | 1                | 5000              |
+--+---------+---------+----------------+-----------------+------------------+-------------------+
+
+WITH table1 AS (
+    SELECT
+       substring(trans_date,1,7) AS month,
+       country,
+       count(state) AS approved_count,
+       sum(amount) AS approved_amount,
+       0 AS chargeback_count,
+       0 AS chargeback_amount
+    FROM Transactions
+    WHERE state = 'approved'
+    GROUP BY month, country
+    UNION
+    SELECT
+        substring(c.trans_date,1,7) AS month,
+        t.country AS country,
+        0 AS approved_count,
+        0 AS approved_amount,
+        count(c.trans_id) AS chargeback_count,
+        sum(t.amount) AS chargeback_amount
+    FROM Chargebacks c
+    LEFT JOIN Transactions t
+    ON c.trans_id = t.id
+    GROUP BY month, country
+)
+SELECT
+    month,
+    country,
+    sum(approved_count) AS approved_count,
+    sum(approved_amount) AS approved_amount,
+    sum(chargeback_count) AS chargeback_count,
+    sum(chargeback_amount) AS chargeback_amount
+FROM table1
+GROUP BY month, country;
 
