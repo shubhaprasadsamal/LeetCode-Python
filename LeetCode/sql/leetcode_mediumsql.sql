87d8b6878fd133bc473b5830320bb79a3a3a0fc9
Shitansu <lat61300@adobe.com>
2022-09-08 18:39:21 -0700

    sql

--- LeetCode/sql/leetcode_mediumsql.sql
+++ LeetCode/sql/leetcode_mediumsql.sql
@@ -4807,3 +4807,103 @@ from
 on
     t.product_id = p.product_id
 where t.rnk = 1;
+
+--1613. Find the Missing IDs
+--Medium
+--
+--158
+--
+--22
+--
+--Add to List
+--
+--Share
+--SQL Schema
+--Table: Customers
+--
+--+---------------+---------+
+--| Column Name   | Type    |
+--+---------------+---------+
+--| customer_id   | int     |
+--| customer_name | varchar |
+--+---------------+---------+
+--customer_id is the primary key for this table.
+--Each row of this table contains the name and the id customer.
+--
+--
+--Write an SQL query to find the missing customer IDs. The missing IDs are ones that are not in the Customers table but are in the range between 1 and the maximum customer_id present in the table.
+--
+--Notice that the maximum customer_id will not exceed 100.
+--
+--Return the result table ordered by ids in ascending order.
+--
+--The query result format is in the following example.
+--
+--
+--
+--Example 1:
+--
+--Input:
+--Customers table:
+--+-------------+---------------+
+--| customer_id | customer_name |
+--+-------------+---------------+
+--| 1           | Alice         |
+--| 4           | Bob           |
+--| 5           | Charlie       |
+--+-------------+---------------+
+--Output:
+--+-----+
+--| ids |
+--+-----+
+--| 2   |
+--| 3   |
+--+-----+
+--Explanation:
+--The maximum customer_id present in the table is 5, so in the range [1,5], IDs 2 and 3 are missing from the table.
+
+-- Recursive:
+# Write your MySQL query statement below
+
+with recursive cte as
+(
+    select 1 as val
+    UNION ALL
+    select
+        val+1
+    from
+        cte
+    where
+        val < (select max(customer_id) from Customers )
+)
+select
+ val as ids
+from
+    cte c1 left join Customers c2
+on
+    c1.val = c2.customer_id
+where
+   c2.customer_id is null ;
+
+-- Need to understand why this solution is getting wrong for some use cases:
+-- #########################################################################
+
+with cte as
+(
+select
+    customer_id ,
+    row_number() over(order by customer_id) as rnk
+
+from
+    Customers
+)
+# select * from cte
+
+select
+    case when customer_id = rnk then 1 else rnk end as ids
+from
+    cte
+where
+	case when customer_id = rnk then 1 else rnk end is not null;
+
+
