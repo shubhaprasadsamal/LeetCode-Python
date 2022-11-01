588518f3bac97de0b81f4d1e50a585bbb17dbd68
Shitansu <lat61300@adobe.com>
2022-11-01 00:38:41 -0700

    sql

--- LeetCode/python/Leetcode_Easy.py
+++ LeetCode/python/Leetcode_Easy.py
@@ -1118,6 +1118,50 @@ class Solution:
             Tri.append(row)
         return Tri
 
+# 119. Pascal's Triangle II
+# Easy
+#
+# 3256
+#
+# 283
+#
+# Add to List
+#
+# Share
+# Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+#
+# In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+#
+#
+#
+#
+# Example 1:
+#
+# Input: rowIndex = 3
+# Output: [1,3,3,1]
+# Example 2:
+#
+# Input: rowIndex = 0
+# Output: [1]
+# Example 3:
+#
+# Input: rowIndex = 1
+# Output: [1,1]
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+
+
+        Tri = [[1]]
+        numidx = rowIndex+1
+        for i in range(1,numidx):
+            row = [1]
+            for j in range(1,i):
+                row.append(Tri[i-1][j-1] + Tri[i-1][j])
+            row.append(1)
+            Tri.append(row)
+        return Tri[rowIndex]
+
 
 
 
