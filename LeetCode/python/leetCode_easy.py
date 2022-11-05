78f97caf3356653149d3d50d07d60902e59d43a7
Shitansu <lat61300@adobe.com>
2022-11-05 12:11:10 -0700

    sql

--- LeetCode/python/leetCode_easy.py
+++ LeetCode/python/leetCode_easy.py
@@ -1340,6 +1340,80 @@ class Solution(object):
 
 # Space complexity : O(1).
 
+# 163. Missing Ranges
+# Easy
+# 868
+# 2.7K
+# company
+# Facebook
+# company
+# ByteDance
+# company
+# TikTok
+# You are given an inclusive range [lower, upper] and a sorted unique integer array nums, where all elements are in the inclusive range.
+#
+# A number x is considered missing if x is in the range [lower, upper] and x is not in nums.
+#
+# Return the smallest sorted list of ranges that cover every missing number exactly. That is, no element of nums is in any of the ranges, and each missing number is in one of the ranges.
+#
+# Each range [a,b] in the list should be output as:
+#
+# "a->b" if a != b
+# "a" if a == b
+#
+#
+# Example 1:
+#
+# Input: nums = [0,1,3,50,75], lower = 0, upper = 99
+# Output: ["2","4->49","51->74","76->99"]
+# Explanation: The ranges are:
+# [2,2] --> "2"
+# [4,49] --> "4->49"
+# [51,74] --> "51->74"
+# [76,99] --> "76->99"
+# Example 2:
+#
+# Input: nums = [-1], lower = -1, upper = -1
+# Output: []
+# Explanation: There are no missing ranges since there are no missing numbers.
+
+class Solution:
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
+
+        res = []
+        nums.append(upper+1)
+        for i in range(len(nums)):
+
+            if lower == nums[i]:
+                pass
+            else:
+                if nums[i] == lower+1:
+                    res.append(str(lower))
+                else:
+                    res.append(str(lower)+"->"+str(nums[i]-1))
+
+
+            lower = nums[i]+1
+
+        return res
+
+class Solution:
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
+
+        sol = []
+        nums = [lower-1] + nums + [upper+1]
+
+        for i in range(len(nums)-1):
+
+            dif = nums[i+1]- nums[i]
+
+            if dif ==2:
+                sol.append(str(nums[i]+1))
+            if dif >2:
+                sol.append(str(nums[i]+1) + '->' + str(nums[i+1]-1) )
+
+        return sol
+
 
 
 
